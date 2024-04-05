1fa0ba49eb8d012cad6d532ca26e41f461ecec7d
Lucas <super.ghast8@gmail.com>
2024-04-05 13:55:00 -0300

    Defining the page layout grid

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
