a0faee700b36f1399f3d890e7d4843d6eb6e8772
Lucas <super.ghast8@gmail.com>
2024-04-08 09:30:45 -0300

    create "itemText" variable and add an input decorator to it

--- ts/src/app/components/sidebar/menu-item/menu-item.component.ts
+++ ts/src/app/components/sidebar/menu-item/menu-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-item',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-item.component.scss']
 })
 export class MenuItemComponent implements OnInit {
+
+  @Input()
+  itemText:string = ""
 
   constructor() { }
 
