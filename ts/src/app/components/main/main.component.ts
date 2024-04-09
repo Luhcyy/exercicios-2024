00c47e969bd89ddd44c356cd9f63cae55506cfe9
Lucas <super.ghast8@gmail.com>
2024-04-08 22:18:21 -0300

    add logic to hide and show the additional text from the summary

--- ts/src/app/components/main/main.component.ts
+++ ts/src/app/components/main/main.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+ selector: 'app-main',
+ templateUrl: './main.component.html',
+ styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-  videoPath:string
-  videoImagePath:string
+ videoPath: string;
+ videoImagePath: string;
+ hideText: boolean = true // Propriedade para controlar a visibilidade do texto adicional
+ showMoreText: boolean = false; // Propriedade para controlar a visibilidade do texto adicional
 
-  constructor() {
-    this.videoPath = "/assets/video.png"
-    this.videoImagePath = "/assets/master_photo-cropped.png"
-  }
+ constructor() {
+    this.videoPath = "/assets/video.png";
+    this.videoImagePath = "/assets/master_photo-cropped.png";
+ }
 
-  ngOnInit(): void {
-  }
+ ngOnInit(): void {
+ }
+
+ toggleShowMoreText() {
+    this.showMoreText = !this.showMoreText; // Alterna o estado da visibilidade do texto
+    this.hideText = !this.hideText; // Alterna o estado da visibilidade do texto
+ }
 
 }
