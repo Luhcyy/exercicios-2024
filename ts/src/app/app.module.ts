c66a8d3d62b06e4bf539f37146bf95cce23b1d15
Lucas <super.ghast8@gmail.com>
2024-04-09 12:15:11 -0300

    add AnsweredTopicComponent to app.module

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { MainComponent } from './components/main/main.component';
 import { MenuItemComponent } from './components/sidebar/menu-item/menu-item.component';
 import { TopicCardComponent } from './components/main/topic-card/topic-card.component';
+import { AnsweredTopicComponent } from './components/main/topic-card/answered-topic/answered-topic.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { TopicCardComponent } from './components/main/topic-card/topic-card.comp
     SidebarComponent,
     MainComponent,
     MenuItemComponent,
-    TopicCardComponent
+    TopicCardComponent,
+    AnsweredTopicComponent
   ],
   imports: [
     BrowserModule
