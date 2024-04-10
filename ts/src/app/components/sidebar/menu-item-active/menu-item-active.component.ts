b19098212e0e369b991feec497f04e2a171363ff
Lucas <super.ghast8@gmail.com>
2024-04-10 20:40:20 -0300

    add input to itemText from menu-item-active

--- ts/src/app/components/sidebar/menu-item-active/menu-item-active.component.ts
+++ ts/src/app/components/sidebar/menu-item-active/menu-item-active.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-menu-item-active',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-item-active.component.scss']
 })
 export class MenuItemActiveComponent implements OnInit {
+
+  @Input()
+  itemText:string = ""
 
   constructor() { }
 
