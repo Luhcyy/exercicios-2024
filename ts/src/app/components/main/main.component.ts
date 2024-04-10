33522397947243d3042d1e245caccc0fedb17bc7
Lucas <super.ghast8@gmail.com>
2024-04-10 01:44:40 -0300

    adds function to hide the Discussions section

--- ts/src/app/components/main/main.component.ts
+++ ts/src/app/components/main/main.component.ts
@@ -1,27 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
- selector: 'app-main',
- templateUrl: './main.component.html',
- styleUrls: ['./main.component.scss']
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
- videoPath: string;
- videoImagePath: string;
- hideText: boolean = true // Propriedade para controlar a visibilidade do texto adicional
- showMoreText: boolean = false; // Propriedade para controlar a visibilidade do texto adicional
+  videoPath: string;
+  videoImagePath: string;
+  hideText: boolean = true // Propriedade para controlar a visibilidade do texto adicional
+  showMoreText: boolean = false; // Propriedade para controlar a visibilidade do texto adicional
+  showDiscussionsSection = true;
+  showTopicForm = false;
+  
 
- constructor() {
+  constructor() {
     this.videoPath = "/assets/video.png";
     this.videoImagePath = "/assets/master_photo-cropped.png";
- }
+}
 
- ngOnInit(): void {
- }
+  ngOnInit(): void {
+}
 
- toggleShowMoreText() {
-    this.showMoreText = !this.showMoreText; // Alterna o estado da visibilidade do texto
+toggleShowMoreText() {
+   this.showMoreText = !this.showMoreText; // Alterna o estado da visibilidade do texto
     this.hideText = !this.hideText; // Alterna o estado da visibilidade do texto
- }
+}
+
+toggleTopicForm () {
+  this.showDiscussionsSection = !this.showDiscussionsSection;
+  this.showTopicForm = !this.showTopicForm;
+}
+
 
 }
