ada446b1bab7408623791265725e9cbd7b221eb7
Lucas <super.ghast8@gmail.com>
2024-04-10 01:47:23 -0300

    creation of the topic-sent component

---
+++ ts/src/app/components/main/topic-sent/topic-sent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topic-sent',
+  templateUrl: './topic-sent.component.html',
+  styleUrls: ['./topic-sent.component.scss']
+})
+export class TopicSentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
