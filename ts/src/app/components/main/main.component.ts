3f21bafef6a912ec325346e7fcd86adf28ca67a2
Lucas <super.ghast8@gmail.com>
2024-04-10 10:42:27 -0300

    remove url method not being used and add function to add pending-topic components

--- ts/src/app/components/main/main.component.ts
+++ ts/src/app/components/main/main.component.ts
@@ -1,35 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-  videoPath: string;
-  videoImagePath: string;
   hideText: boolean = true // Propriedade para controlar a visibilidade do texto adicional
   showMoreText: boolean = false; // Propriedade para controlar a visibilidade do texto adicional
   showDiscussionsSection = true;
   showTopicForm = false;
   
+  pendingTopics: any[] = [];
+  
 
   constructor() {
-    this.videoPath = "/assets/video.png";
-    this.videoImagePath = "/assets/master_photo-cropped.png";
+
 }
 
   ngOnInit(): void {
 }
 
 toggleShowMoreText() {
-   this.showMoreText = !this.showMoreText; // Alterna o estado da visibilidade do texto
-    this.hideText = !this.hideText; // Alterna o estado da visibilidade do texto
+    this.showMoreText = !this.showMoreText; 
+    this.hideText = !this.hideText; 
 }
 
 toggleTopicForm () {
   this.showDiscussionsSection = !this.showDiscussionsSection;
   this.showTopicForm = !this.showTopicForm;
+}
+
+addPendingTopic() {
+  this.pendingTopics.push({})
 }
 
 
