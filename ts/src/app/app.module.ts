d5ae7ad80b0b53832957036fa0501991bc86c5ee
Lucas <super.ghast8@gmail.com>
2024-04-10 01:41:00 -0300

    adds topic-form and topic-sent components to the app module

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
@@ -8,7 +7,9 @@ import { MainComponent } from './components/main/main.component';
 import { MenuItemComponent } from './components/sidebar/menu-item/menu-item.component';
 import { TopicCardComponent } from './components/main/topic-card/topic-card.component';
 import { AnsweredTopicComponent } from './components/main/topic-card/answered-topic/answered-topic.component';
-import { TopicFormComponent } from './components/main/topic-card/topic-form/topic-form.component';
+import { TopicFormComponent } from './components/main/topic-form/topic-form.component';
+import { TopicSentComponent } from './components/main/topic-sent/topic-sent.component';
+
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { TopicFormComponent } from './components/main/topic-card/topic-form/topi
     MenuItemComponent,
     TopicCardComponent,
     AnsweredTopicComponent,
-    TopicFormComponent
+    TopicFormComponent,
+    TopicSentComponent
   ],
   imports: [
     BrowserModule
