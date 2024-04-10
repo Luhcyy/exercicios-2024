70834d39c1d813ef1495681676f1275af7f697a5
Lucas <super.ghast8@gmail.com>
2024-04-10 17:15:16 -0300

    adds style to topic-sent and more elements to it's html

--- ts/src/app/components/main/topic-sent/topic-sent.component.ts
+++ ts/src/app/components/main/topic-sent/topic-sent.component.ts
@@ -12,4 +12,6 @@ export class TopicSentComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  
 }
--- ts/src/app/components/sidebar/sidebar.component.ts
+++ ts/src/app/components/sidebar/sidebar.component.ts
@@ -13,7 +13,9 @@ export class SidebarComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    
+
   }
 
 }
+
+
